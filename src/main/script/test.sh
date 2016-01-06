f2eb3a3b9d1466db8f2ad1fda5f7533f860a72bf
jarvis-su001 <jarvis_su@126.com>
2016-01-06 14:33:35 +0800

    add readonly test

--- src/main/script/test.sh
+++ src/main/script/test.sh
@@ -2,3 +2,8 @@
 echo "hello, input your name  "
 read PERSON
 echo "hello, $PERSON"
+
+FINAL_STRING=$JAVA_HOME
+readonly FINAL_STRING
+echo "FINAL_STRING can not be modified, its value is $FINAL_STRING"
+
